f6e1b4ff007a00fc878cce977611bdf34903d6d6
doctor3397 <hs2546@columbia.edu>
2017-02-06 17:24:45 -0500

    Add nested routes for products and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
-  get 'reviews/show'
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
 
-  get 'reviews/create'
-
-  get 'reviews/destroy'
-
-  resources :products
   resource :users, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
 end
+
+
+# Prefix Verb   URI Pattern                                 Controller#Action
+# product_reviews POST  /products/:product_id/reviews(.:format)     reviews#create
+# product_review GET    /products/:product_id/reviews/:id(.:format) reviews#show
+#                DELETE /products/:product_id/reviews/:id(.:format) reviews#destroy
+#       products GET    /products(.:format)                         products#index
+#                POST   /products(.:format)                         products#create
+#    new_product GET    /products/new(.:format)                     products#new
+#   edit_product GET    /products/:id/edit(.:format)                products#edit
+#        product GET    /products/:id(.:format)                     products#show
+#                PATCH  /products/:id(.:format)                     products#update
+#                PUT    /products/:id(.:format)                     products#update
+#                DELETE /products/:id(.:format)                     products#destroy
+#          users POST   /users(.:format)                            users#create
+#      new_users GET    /users/new(.:format)                        users#new
+#       sessions POST   /sessions(.:format)                         sessions#create
+#   new_sessions GET    /sessions/new(.:format)                     sessions#new
+#                DELETE /sessions(.:format)                         sessions#destroy

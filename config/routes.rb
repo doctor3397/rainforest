210edbb6e4ee5c9566b3ba22bfe806c9e6dd58ce
doctor3397 <hs2546@columbia.edu>
2017-02-06 17:20:16 -0500

    Create a review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/show'
+
+  get 'reviews/create'
+
+  get 'reviews/destroy'
+
   resources :products
   resource :users, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]

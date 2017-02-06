17f9613953271a40636460188cdb886788359e37
doctor3397 <hs2546@columbia.edu>
2017-02-06 17:57:46 -0500

    Implement the show,create, destroy methods ReviewController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,39 @@
 class ReviewsController < ApplicationController
+
+  before_action :load_product
+
+
   def show
+    @review = Review.find(params[:id])
   end
 
   def create
+    @review = Review.new(
+      comment: review_params[:review][:comment],
+      product_id: @product.id,
+      user_id: current_user.id
+    )
+
+    if @review.save
+      redirect_to products_url, notice: 'Review created successfully'
+    else
+      render 'products/show'
+    end
+
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
   end
+
+  private
+  def review_params
+    params.require(:review).permit(:comment, :product_id)
+  end
+
+  def load_product
+    @product = Product.find(params[:product_id])
+  end
+
 end
